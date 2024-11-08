8d3f112b34ae1592386e57ea5d17ebb4313a6ffc
Kabilan <kabilan185@gmail.com>
2024-11-08 23:05:20 +0530

    Worked on retreving the file from the storage and combining the chunks

--- models/Chunk.go
+++ models/Chunk.go
@@ -1,9 +1,10 @@
 package models
 
 type Chunk struct {
-	ChunkId    int    `json:"chunk_id"`
-	FileId     int    `json:"file_id"`
-	ChunkIndex string `json:"chunk_index"`
+	ChunkId    int64  `json:"chunk_id"`
+	FileId     int64  `json:"file_id"`
+	Port       int    `json:"port"`
+	ChunkIndex int    `json:"chunk_index"`
 	ChunkSize  string `json:"chunk_size"`
 	ChunkHash  string `json:"chunk_hash"`
 }
